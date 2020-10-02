8e718707e4033e7627e554975441f55333c4dc85
saiful alam <saiful@digitalnexa.com>
2020-10-02 16:46:03 +0400

    Add project files.

---
+++ SignalRChat/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Models
+{
+    public class Message
+    {
+        public string Title { get; set; }
+        public string MessageDescription { get; set; }
+    }
+}
---
+++ SignalRChat/wwwroot/js/Message.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/Messagehub").build();
+connection.on("sendToUser", (title, message) => {
+    var heading = document.createElement("h3");
+    heading.textContent = title;
+    var p = document.createElement("p");
+    p.innerText = message;
+
+    var div = document.createElement("div");
+    div.appendChild(heading);
+    div.appendChild(p);
+
+    document.getElementById("messageList").appendChild(div);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
